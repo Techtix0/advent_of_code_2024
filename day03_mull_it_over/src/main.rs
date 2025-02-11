cdc57c982c06328670ed49d225ead1d26ca45f3b
Techtix0 <Techtix0@proton.me>
2025-02-11 11:00:24 +0100

    Finished part 1

--- day03_mull_it_over/src/main.rs
+++ day03_mull_it_over/src/main.rs
@@ -3,20 +3,42 @@ use std::fs;
 
 fn main() {
     let contents = get_input();
-    let test_string = String::from(r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
-    let matches = find_valid_mul(&test_string);
+    let mut lines = contents.lines();
+    let mut line = lines.next();
+    let mut mults: Vec<&str> = vec![];
 
-    for m in matches {
-        println!("{m}");
+    while line != None {
+        let matches = find_valid_mul(&line.unwrap());
+        for m in matches {
+            mults.push(m);
+        }
+        line = lines.next();
     }
+
+    println!("{}", calculate(mults));
 }
 
 fn get_input() -> String {
     return fs::read_to_string("input.txt").expect("Error reading file");
 }
 
-fn find_valid_mul(line: &String) -> Vec<&str> {
+fn find_valid_mul(line: &str) -> Vec<&str> {
     let pattern = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
     let matches: Vec<&str> = pattern.find_iter(&line).map(|m| m.as_str()).collect();
     matches
+}
+
+fn calculate(mults: Vec<&str>) -> i32 {
+    let mut calcs: Vec<i32> = vec![];
+
+    for mult in mults {
+        calcs.push(
+            mult.replace(r"mul(", "")
+                .replace(r")", "")
+                .split(",")
+                .map(|num| num.parse::<i32>().unwrap())
+                .fold(1, |acc, x| acc * x)
+        );
+    }
+    calcs.iter().sum()
 }
