e38dc7e80d29d8648867b5115f420f0ed2377ca4
Techtix0 <Techtix0@proton.me>
2025-02-11 10:17:18 +0100

    Added regex function to search for correct patterns

--- day03_mull_it_over/src/main.rs
+++ day03_mull_it_over/src/main.rs
@@ -1,2 +1,22 @@
+use regex::Regex;
+use std::fs;
+
 fn main() {
+    let contents = get_input();
+    let test_string = String::from(r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+    let matches = find_valid_mul(&test_string);
+
+    for m in matches {
+        println!("{m}");
+    }
+}
+
+fn get_input() -> String {
+    return fs::read_to_string("input.txt").expect("Error reading file");
+}
+
+fn find_valid_mul(line: &String) -> Vec<&str> {
+    let pattern = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let matches: Vec<&str> = pattern.find_iter(&line).map(|m| m.as_str()).collect();
+    matches
 }
